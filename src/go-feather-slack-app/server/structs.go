be90dc1af3d9f517e33ba2e35d8cfe4736dc1dc2
saisona <alexandre.saison.pro@gmail.com>
2020-12-28 19:29:36 +0100

    Update : adding cleanup as parameter
     setting it as true will cleanup job right after finished

--- src/go-feather-slack-app/server/structs.go
+++ src/go-feather-slack-app/server/structs.go
@@ -2,7 +2,7 @@
  * File              : structs.go
  * Author            : Alexandre Saison <alexandre.saison@inarix.com>
  * Date              : 21.12.2020
- * Last Modified Date: 21.12.2020
+ * Last Modified Date: 28.12.2020
  * Last Modified By  : Alexandre Saison <alexandre.saison@inarix.com>
  */
 
@@ -20,8 +20,14 @@ type JobCreationPayload struct {
 	JobName         string   `json:"jobName"`
 	ConfigMapsNames []string `json:"configMapsNames"`
 	DockerImage     string   `json:"dockerImage"`
+	CleanUp         bool     `json:"cleanup"`
 }
 
 type PodsRequestPayload struct {
 	Namespace string `json:"namespace"`
+}
+
+type PodRequestPayload struct {
+	Namespace string `json:"namespace"`
+	Name      string `json:"name"`
 }
