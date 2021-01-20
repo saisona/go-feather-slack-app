9f23544e6b0cf2f08e0284010fdaf927d58a79dd
saisona <alexandre.saison.pro@gmail.com>
2021-01-20 15:40:44 +0100

    WIP : some test with configuration

--- src/go-feather-slack-app/server/utils.go
+++ src/go-feather-slack-app/server/utils.go
@@ -2,18 +2,43 @@
  * File              : utils.go
  * Author            : Alexandre Saison <alexandre.saison@inarix.com>
  * Date              : 04.01.2021
- * Last Modified Date: 04.01.2021
+ * Last Modified Date: 20.01.2021
  * Last Modified By  : Alexandre Saison <alexandre.saison@inarix.com>
  */
 package server
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/slack-go/slack"
 )
+
+func sendStatusMethodNotAllowed(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusMethodNotAllowed)
+	w.Write(nil)
+}
+
+func sendStatusInternalError(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusInternalServerError)
+	fmt.Fprintln(w, "An error occured please look at the logs !")
+}
+
+func fromBodyToStruct(httpBody io.ReadCloser, structHandler interface{}) error {
+	body, err := ioutil.ReadAll(httpBody)
+	if err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal(body, &structHandler); err != nil {
+		return err
+	}
+	return nil
+}
 
 func SendSlackMessage(message string, w http.ResponseWriter) {
 	params := &slack.Msg{Text: message}
