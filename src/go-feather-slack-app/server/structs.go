8fb34a03e461c8d6e2f831ebc3c5864d95054e93
saisona <alexandre.saison.pro@gmail.com>
2021-01-20 15:40:44 +0100

    WIP : some test with configuration

--- src/go-feather-slack-app/server/structs.go
+++ src/go-feather-slack-app/server/structs.go
@@ -2,7 +2,7 @@
  * File              : structs.go
  * Author            : Alexandre Saison <alexandre.saison@inarix.com>
  * Date              : 21.12.2020
- * Last Modified Date: 29.12.2020
+ * Last Modified Date: 20.01.2021
  * Last Modified By  : Alexandre Saison <alexandre.saison@inarix.com>
  */
 
@@ -12,13 +12,11 @@ import (
 	PodManager "github.com/saisona/go-feather-slack-app/src/go-feather-slack-app/manager"
 )
 
-//const SLACK_API_TOKEN string := os.Getenv("SLACK_API_TOKEN")
-//TODO : add SLACK_API_TOKEN !
-
 type Server struct {
 	port            int
 	manager         PodManager.PodManager
 	SLACK_API_TOKEN string
+	DOCKER_IMAGE    string
 }
 
 type JobCreationPayload struct {
